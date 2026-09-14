dde2fbb232319d793b00c40306b8bd1e3e3acfdf
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-09-15 00:50:30 +0300

    Some cleanup and readd JS

---
+++ posts/index.js
@@ -0,0 +1,34 @@
+/** @type {HTMLTableSectionElement} */
+const post_data = document.getElementById("post-data");
+
+/** @type {HTMLParagraphElement} */
+const count_field = document.getElementById("post-count-field")
+
+/** @type {HTMLInputElement} */
+const search = document.getElementById("searchbar");
+
+/** @type {HTMLDivElement} */
+const result_container = document.getElementById("result-container");
+
+const js_enabled = document.querySelectorAll(".hidden-when-js-unavailable");
+
+const all_nodes = document.createDocumentFragment()
+
+Array.from(post_data.childNodes).forEach(child => all_nodes.appendChild(child.cloneNode(true)));
+
+search.addEventListener('input', e => {
+    /** @type {string} */
+    let query = e.target.value.toLowerCase();
+
+    post_data.replaceChildren();
+    Array.from(all_nodes.childNodes).filter(child => child.textContent.toLowerCase().includes(query)).forEach(child => post_data.appendChild(child.cloneNode(true)));
+    count_field.innerText = `${post_data.childElementCount} Gönderi`
+    if (post_data.childElementCount == 0) {
+        result_container.style.display = "none";
+    } else {
+        result_container.style.display = "block";
+    }
+});
+
+console.log(js_enabled)
+js_enabled.forEach(element => element.classList.remove("hidden-when-js-unavailable"));
